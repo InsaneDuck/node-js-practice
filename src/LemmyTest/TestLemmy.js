ef426a08937d9064bead048a5391a214be1561ad
satya <39309626+InsaneDuck@users.noreply.github.com>
2023-08-03 01:39:54 +0100

    updated

--- src/LemmyTest/TestLemmy.js
+++ src/LemmyTest/TestLemmy.js
@@ -1,21 +1,17 @@
 import {LemmyHttp} from "lemmy-js-client";
 import * as fs from "fs";
 
-let baseUrl = 'https://lemmy.ml/';
+let baseUrl = 'https://lemmy.world/';
 let client = new LemmyHttp(baseUrl, {});
-let loginForm = {
-    username_or_email: "insaneduck",
-    password: "Login1234%",
-};
 
 let comments = 'comments';
 let posts = 'posts';
 let post = 'post'
 await client.getPosts({page:1,limit:50,sort:"Active"}).then(data => posts=JSON.stringify(data)).catch(error=>console.log(error))
 writeToFile('posts.json',posts)
-await client.getPost({id:2453149}).then(data=> post = JSON.stringify(data))
+await client.getPost({id:2338172}).then(data=> post = JSON.stringify(data))
 writeToFile('post.json',post)
-await client.getComments({post_id:2453149,}).then(data=> comments = JSON.stringify(data))
+await client.getComments({post_id:2338172,max_depth:10,limit:50,page:1}).then(data=> comments = JSON.stringify(data))
 writeToFile('comments.json',comments)
 function writeToFile(fileName,string)
 {
